1a7e43cc8e4bb93951d5721fbf3724ca7fdd80c7
fr_shinkawa <y_shinkawa@starfactory-inc.co.jp>
2017-09-20 17:08:43 +0900

    unity test

---
+++ Unity/Assets/script/test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class test : MonoBehaviour {
+
+    private void Awake() {
+        Debug.Log("awake");
+
+        //SceneManager.LoadScene("test", LoadSceneMode.Single);
+        SceneManager.LoadScene("background", LoadSceneMode.Additive);
+        SceneManager.LoadScene("gimmick", LoadSceneMode.Additive);
+        SceneManager.LoadScene("ui", LoadSceneMode.Additive);
+
+    }
+
+    void Start () {
+        Debug.Log("play");
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log("update");
+    }
+
+
+}
