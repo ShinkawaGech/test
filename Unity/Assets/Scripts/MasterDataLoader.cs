ce3a834e43741039a82ddceda770ee6109dac4c0
ShinkawaGech <arata_3_@hotmail.com>
2017-09-22 20:04:26 +0900

    フラグをcsvファイルで読み込んでリスト化する

--- Unity/Assets/Scripts/MasterDataLoader.cs
+++ Unity/Assets/Scripts/MasterDataLoader.cs
@@ -16,6 +16,34 @@ public class MasterDataLoader {
             csvDatas.Add(line.Split(','));
         }
 
+        //要素数取得
+        int row = csvDatas.Count;
+        int col = csvDatas[0].Length;
+        //Debug.Log("Lst :" + row + ":" + col);
+
+        //csvデータのディクショナリ可
+        Dictionary<string, bool> flg = new Dictionary<string, bool>();
+
+        for (int i = 0; i < row; i++) {
+            string key = csvDatas[i][0];
+            bool value = bool.Parse(csvDatas[i][1]);
+            flg.Add(key, value);
+        }
+
+        //デバッグ：参照
+        //foreach (KeyValuePair<string, bool> pair in flg) {
+        //Debug.Log(pair.Key + " : " + pair.Value);
+        //}
+        Debug.Log("m01_kit_a : " + flg["m01_kit_a"]);
+        Debug.Log("m01_kit_b : " + flg["m01_kit_b"]);
+        Debug.Log("m01_kit_c : " + flg["m01_kit_c"]);
+
+        //デバッグ：書き込み
+        flg["m01_kit_b"] = true;
+        Debug.Log("m01_kit_a : " + flg["m01_kit_a"]);
+        Debug.Log("m01_kit_b : " + flg["m01_kit_b"] + " <-Trueになる");
+        Debug.Log("m01_kit_c : " + flg["m01_kit_c"]);
+
     }
 
 
--- Unity/Assets/scripts/test.cs
+++ Unity/Assets/scripts/test.cs
@@ -5,21 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class test : MonoBehaviour {
 
-    public string FileName = "test";
-    public MasterDataLoader testData = new MasterDataLoader();
-    
-    void Awake() {
-        testData.CsvRead(FileName);
-        Debug.Log("CSV:" + testData.csvDatas[1][1]);
+    public string FileName = "FlagList";
+    public MasterDataLoader flagData = new MasterDataLoader();
 
-        //for (int i = 0; i < testData.csvDatas.Count ; i++) {
-        //    for (int j = 0; j < testData.csvDatas[i].Length; j++)
+
+
+    void Awake() {
+        //フラグデータ読み込み
+        flagData.CsvRead(FileName);
+
+        //List[行][列] 0列のデータ(ラベル）のみ
+        //Debug.Log("Csv : " + flagData.csvDatas[0][0]);
+        //Debug.Log("Csv : " + flagData.csvDatas[1][0]);
+        //Debug.Log("Csv : " + flagData.csvDatas[82][0]);
+
+        //デバッグ
+        //for (int i = 0; i < flagData.csvDatas.Count ; i++) {
+        //    for (int j = 0; j < flagData.csvDatas[i].Length; j++)
         //    {
-        //        Debug.Log("CSV["+i+"]["+j+"] = " + testData.csvDatas[i][j]);
+        //        Debug.Log("Csv : ["+i+"]["+j+"] = " + flagData.csvDatas[i][j]);
         //    }
         //}
 
         //Debug.Log("awake");
+
+     
 
     }
 
@@ -28,6 +38,9 @@ public class test : MonoBehaviour {
         SceneManager.LoadScene("gimmick", LoadSceneMode.Additive);
         SceneManager.LoadScene("ui", LoadSceneMode.Additive);
 
+        string str = "true";
+        bool a = bool.Parse(str);
+        //Debug.Log(a);   
     }
 
     void Update () {
