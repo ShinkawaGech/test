29af1ca390d0a47c32f0d4725a031a8fdf235f7a
ShinkawaGech <arata_3_@hotmail.com>
2017-10-12 15:25:19 +0900

    カットリスト読み込みテスト

--- Unity/Assets/scripts/System/MasterDataLoader.cs
+++ Unity/Assets/scripts/System/MasterDataLoader.cs
@@ -5,11 +5,8 @@ using System.IO;
 
 public class MasterDataLoader : MonoBehaviour {
 
-
     //csv読み込み用
     public List<string[]> csvDatas = new List<string[]>();
-    public List<string> cutDatas = new List<string>();
-    public List<string> _cutDatas = new List<string>();
 
     //private List<GameObject>	myList		= new List<GameObject>();
     //GameObject newObj2 = myList[0]; //Listへのアクセス
@@ -23,12 +20,12 @@ public class MasterDataLoader : MonoBehaviour {
     void Awake(){
 
 		//マスターデータ読み込み
-	//	LoadData("FlagList");		//フラグ		A:string,bool
-	//	LoadData("EventList");		//イベント		A:string,bool
-	//	LoadData("GimmickList");	//ギミック		B:string,string
-	//	LoadData("ItemList");		//アイテム		B:string,string
-		LoadData("CutList");		//カット		C:string,list[string,string,string]
-	//	LoadData("CameraList");		//カメラリスト	D:string,list[float,float,float, float,float,float]
+		LoadData("FlagList");		//フラグ		    A:string,bool
+		LoadData("EventList");		//イベント		A:string,bool
+		LoadData("GimmickList");	//ギミック		B:string,string
+		LoadData("ItemList");		//アイテム	    B:string,string
+		LoadData("CutList");		//カット	    	C:string,list[string[3]]
+		LoadData("CameraList");		//カメラリスト	D:string,list[float[6]]
 
 	}
 
@@ -66,12 +63,10 @@ public class MasterDataLoader : MonoBehaviour {
                     bool value = bool.Parse(csvDatas[i][1]);
                     FlagDic.Add(key, value);
 
-                    //Debug.Log(fileName + " : " + row + " : " + col);
-                    //Debug.Log("List : " + csvDatas[i][0] + " : " + csvDatas[i][1]);
                     //Debug.Log("Dic[value] : " + FlagDic[csvDatas[i][0]]);
                 }
 
-                Debug.Log(fileName + " : " + row + "件、読込完了！");
+                Debug.Log(fileName + " : " + FlagDic.Count + "件、読込完了！");
                 break;
 
             //A: string,bool
@@ -83,12 +78,10 @@ public class MasterDataLoader : MonoBehaviour {
         		    bool value = bool.Parse(csvDatas[i][1]);
     		        EventDic.Add(key, value);
 
-                    //Debug.Log(fileName + " : " + row + " : " + col);
-                    //Debug.Log("List : " + csvDatas[i][0] + " : " + csvDatas[i][1]);
                     //Debug.Log("Dic[value] : " + EventDic[csvDatas[i][0]]);
                 }
 
-                Debug.Log(fileName + " : " + row + "件、読込完了！");
+                Debug.Log(fileName + " : " + EventDic.Count + "件、読込完了！");
                 break;
 
             //B: string,string
@@ -100,8 +93,6 @@ public class MasterDataLoader : MonoBehaviour {
                     string value = csvDatas[i][1];
                     GimmickDic.Add(key, value);
 
-                    //Debug.Log(fileName + " : " + row + " : " + col);
-                    //Debug.Log("List : " + csvDatas[i][0] + " : " + csvDatas[i][1]);
                     //Debug.Log("Dic[value] : " + GimmickDic[csvDatas[i][0]]);
                 }
 
@@ -117,58 +108,38 @@ public class MasterDataLoader : MonoBehaviour {
                     string value = csvDatas[i][1];
                     ItemDic.Add(key, value);
 
-                    //Debug.Log(fileName + " : " + row + " : " + col);
-                    //Debug.Log("List : " + csvDatas[i][0] + " : " + csvDatas[i][1]);
                     //Debug.Log("Dic[value] : " + ItemDic[csvDatas[i][0]]);
                 }
 
-                Debug.Log(fileName + " : " + row + "件、読込完了！");
+                Debug.Log(fileName + " : " + ItemDic.Count + "件、読込完了！");
                 break;
 
-            //C: string,list<string[]>
+            //C: string,list<string[3]>
             case "CutList":
-                //Dictionary<string, List<string>> CutDic = new Dictionary<string, List<string>>();
-                Dictionary<string, string[]> CutDic = new Dictionary<string, string[]>();
-
-                string[] array = new string[3];
-
-                //string turnL, turnR, turnB;
+                Dictionary<string, List<string[]>> CutDic = new Dictionary<string, List<string[]>>();
 
                 for (int i = 0; i < row; i++)
                 {
+                    //毎回newする事で別のリストになる
+                    List<string[]> cutDatas = new List<string[]>();
+                    string[] array = new string[3];
+
                     string key = csvDatas[i][0];
 
                     array[0] = csvDatas[i][1];
                     array[1] = csvDatas[i][2];
                     array[2] = csvDatas[i][3];
+                    cutDatas.Add(array);
 
-                    cutDatas.Add(array[0]);
-                    cutDatas.Add(array[1]);
-                    cutDatas.Add(array[2]);
+                    CutDic.Add(key, cutDatas);
 
-                    //turnL = csvDatas[i][1];
-                    //turnR = csvDatas[i][2];
-                    //turnB = csvDatas[i][3];
-
-                    //cutDatas.Add(turnL);
-                    //cutDatas.Add(turnR);
-                    //cutDatas.Add(turnB);
-
-                    //_cutDatas = cutDatas;
-
-                    //CutDic.Add(key, cutDatas);
-                    Debug.Log(key + " : " + CutDic[key][0] + " : " + CutDic[key][1] + " : " + CutDic[key][2]);
-                    Debug.Log(_cutDatas.Count);
-
-                    //専用リストなので消さない。リスト丸ごと入っているので初期化するとDic内も消える
-                    //消さないとひと繋ぎのリストになってしまう
-                    //string[]配列にする->駄目
-                    cutDatas.Clear();
-
+                    //専用リストなので消さない。
+                    //cutDatas.Clear();
                 }
-                //Debug.Log(CutDic["room_box_01"][1]);
-                //Debug.Log(CutDic["room_box_01"][2]);
-                Debug.Log(fileName + " : " + row + "件、読込完了！");
+
+                //[key][0固定][0:L,1:R,2:B]
+                //Debug.Log(CutDic["room_front"][0][0]);
+                Debug.Log(fileName + " : " + CutDic.Count + "件、読込完了！");
                 break;
 
             //C: string,float[]
@@ -177,6 +148,7 @@ public class MasterDataLoader : MonoBehaviour {
                 //Dictionary<string, string[]> CutList = new Dictionary<string, strinList<string>>();
 
 
+                Debug.Log("カメラリスト");
 
                 break;
 
